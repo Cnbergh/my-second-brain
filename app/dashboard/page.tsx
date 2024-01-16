ff0376cfbbe94c5aef920e170eb50cfd11787b63
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2024-01-16 18:59:40 +0100

    Test ok towards api DB

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,15 @@
+import { getXataClient } from "@/xata";
 import React from "react";
 
-export default function DashboardPage() {
-    return <div>Dashboard page</div>
+export default async function DashboardPage() {
+    const xataClient = getXataClient();
+    const tasks = await xataClient.db.task.getMany();
+    return <div>
+        <h1>Dashboard page</h1>
+        <div>
+            {tasks.map((task) =>(
+                <p key={task.id}>{task.name}</p>
+            ))}
+        </div>
+        </div>
 }
